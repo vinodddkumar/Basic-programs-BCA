764416b08a3ea9d350d46ce1fe9823cea6dda78e
Vinod kumar <vinodddkumar2005@gmail.com>
2025-12-16 19:18:19 +0530

    Update Student.sql

--- Sql/Student.sql
+++ Sql/Student.sql
@@ -3,3 +3,23 @@ CREATE TABLE Student (
     name VARCHAR(50),
     marks INT
 );
+
+-- Insert student records
+INSERT INTO student VALUES (1, 'Ravi', 'BCA', 78);
+INSERT INTO student VALUES (2, 'Anita', 'BCA', 85);
+INSERT INTO student VALUES (3, 'Kiran', 'BCA', 62);
+
+-- Display all students
+SELECT * FROM student;
+
+-- Students scoring more than 70
+SELECT name, marks FROM student WHERE marks > 70;
+
+-- Update marks of a student
+UPDATE student SET marks = 90 WHERE student_id = 1;
+
+-- Delete a student record
+DELETE FROM student WHERE student_id = 3;
+
+-- Remove table
+DROP TABLE student;
